a275e8acf12b7f5792ea4e00218c909ae0e21524
victor_ahumah <victor.ahumah@financemobilesuite.com>
2024-02-02 12:25:49 +0000

    update config file

--- config/settings/base.py
+++ config/settings/base.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "djmoney",
     "django_filters",
     "phonenumber_field", 
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
