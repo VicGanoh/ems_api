b835fc89e0d1c0386ba78c548078064accfd8043
Victor Tetteh-Ganoh Ahumah <kofiganoh@gmail.com>
2024-04-07 14:03:36 +0000

    update urls.py

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -7,12 +7,10 @@ urlpatterns = format_suffix_patterns(
     [
         path("auth/signup/", views.RegistrationView.as_view(), name="signup"),
         path("auth/login/", views.LoginView.as_view(), name="login"),
-        path("authorize/", views.OAuth2LoginInView.as_view(), name="oauth2login"),
-        path("callback/", views.OAuth2Callback.as_view(), name="callback"),
         path(
-            "password_update/",
-            views.UpdatePasswordView.as_view(),
-            name="update_password",
+            "change-password/",
+            views.ChangePasswordView.as_view(),
+            name="change-password",
         ),
         path("users", views.ListUsersView.as_view(), name="users"),
         path("users/<uuid:pk>", views.RetrieveDestroyUserView.as_view()),
